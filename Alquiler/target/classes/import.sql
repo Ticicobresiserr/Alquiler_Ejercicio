78e40deff6321a7b4716ca3b44e0efe0649dfdf7
ticicobresiserr <tici.cobresi21@gmail.com>
2020-10-21 00:45:18 -0300

    borro metadata

--- Alquiler/target/classes/import.sql
+++
@@ -1,7 +0,0 @@
--- You can use this file to load seed data into the database using SQL statements
-
-insert into PELICULA (TITULO, SINOPSIS, ANIO, GENERO) values ('Nemo','buena', 2005, 0);
-
-insert into ALQUILER (FECHA_ALQUILER, SOCIO_ID, PELICULA_ID) values ('10/10/2020', 1, 1);
-
-insert into SOCIO (NOMBRE , APELLIDO , DNI, EMAIL) values ('Ticiana', 'Cobresi', '42358387', 'tici.cobresi21@gmail.com');
--- Alquiler/target/test-classes/import.sql
+++
@@ -1,8 +0,0 @@
--- You can use this file to load seed data into the database using SQL statements
-
-insert into PELICULA (titulo, sinopsis, anio, genero) values ('Nemo', 'buena', 2005, 'animada');
-insert into PELICULA (titulo, sinopsis, anio, genero) values ('Bambi', 'mala', 2003, 'animada');
-
-insert into ALQUILER (fecha_alquiler, socio) values (10/10/2020, 1);
-
-insert into SOCIO (nombre,apellido,dni, email) values ('Ticiana', 'Cobresi', 42358387, 'tici.cobresi21@gmail.com');
